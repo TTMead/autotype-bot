a3864d8e504826439d9de51e20a9793a3a3e1fd4
TTMead <timothy.mead20@gmail.com>
2022-01-07 23:04:03 +1000

    Added the ability to print custom text

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import keyboard
 import time
+import tkinter as tk
+from tkinter import simpledialog
+
 running = True
+
+# Set up q to quit the program loop
 def quit():
     global running
     running = False
-
 keyboard.add_hotkey('q',quit)
+
+# Setup tkinter root process
+root = tk.Tk()
+root.withdraw()
+
+# Prompt for input to write
+text_to_write = simpledialog.askstring(title="Prompt",
+                                  prompt="What text would you like to write?:")
+
+# Delay before starting loop
 time.sleep(3)
 
+# Typing loop
 while running:
-    keyboard.write("Ganesh's Favourite word\n",delay=0)
+    keyboard.write(text_to_write + "\n", delay=0)
     time.sleep(0.1)
-
